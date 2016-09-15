7a522e843fcef87613e40fc4cbc3da6ca32c8d70
Igor Golubenkov <i.golubenkov@mail.ru>
2016-09-15 14:28:41 +0300

    Add several function

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -3,8 +3,23 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgramm {
 
     public static void main(String[] args) {
+        hello("world");
 
-        System.out.println("Test");
+        double len = 5;
+        System.out.println("Area of a square, with parties = " + len + " = " + area(len));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Area oa a rectangle, with parties = " + a + " and " + b + " = " + area(a, b));
+    }
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+    public static double area(double l) {
+        return l*l;
+    }
+    public static double area(double a, double b) {
+        return a*b;
     }
 }
 

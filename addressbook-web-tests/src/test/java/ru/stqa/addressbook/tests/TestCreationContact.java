717ab589cc4cab3f80a8ac5d3fb99d15eeff8540
Igor Golubenkov <i.golubenkov@mail.ru>
2016-09-21 21:19:54 +0300

    fixed TestCreationContact add step openHomePage

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestCreationContact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestCreationContact.java
@@ -6,10 +6,11 @@ import ru.stqa.addressbook.model.ContactData;
 public class TestCreationContact extends TestBase{
 
     @Test
-    public void testCreationContact() throws InterruptedException {
+    public void testCreationContact() {
         app.getNavigationHelper().goToAddandEditContactPage();
-        app.getContactHelper().fillGroupForm(new ContactData("test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8"));
+        app.getContactHelper().fillGroupForm(new ContactData("test1", "test2", "test3",
+                "test4", "test5", "test6", "test7", "test8"));
         app.getContactHelper().submitContactCreation();
-        Thread.sleep(3000);
+        app.getNavigationHelper().goToHomePage();
         }
 }

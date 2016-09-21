9e4fd09c439939e248e93d209b074b73f7014794
Igor Golubenkov <i.golubenkov@mail.ru>
2016-09-21 12:58:24 +0300

    edit locator group page submit

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/GroupHelper.java
@@ -10,18 +10,16 @@ public class GroupHelper extends HelperBase{
         super(wd);
     }
     public void returnToGroupPage() {
-        click(By.linkText("group page"));
+        click(By.xpath("//a[contains(.,'group page')]"));
     }
     public void submitGroupCreation() {
         click(By.name("submit"));
     }
-
     public void fillGroupForm(GroupData groupData) {
         type(By.name("group_name"), groupData.getName());
         type(By.name("group_header"), groupData.getHeader());
         type(By.name("group_footer"), groupData.getFooter());
     }
-
     public void initGroupCreation() {
         click(By.name("new"));
     }
@@ -30,5 +28,12 @@ public class GroupHelper extends HelperBase{
     }
     public void selectGroup() {
         click(By.xpath(".//*[@id='content']/form/span[1]/input"));
+    }
+
+    public void initGroupModification() {
+        click(By.xpath(".//*[@id='content']/form/input[3]"));
+    }
+    public void submitGroupModification() {
+        click(By.xpath("//input[@name='update']"));
     }
 }

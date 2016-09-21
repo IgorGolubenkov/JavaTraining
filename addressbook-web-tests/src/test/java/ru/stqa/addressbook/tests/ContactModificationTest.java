5ade0e2ae0af476d9c8ab7353ab5af0c851dc6bd
Igor Golubenkov <i.golubenkov@mail.ru>
2016-09-21 21:23:14 +0300

    add testContactModification

---
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/ContactModificationTest.java
@@ -0,0 +1,16 @@
+package ru.stqa.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.addressbook.model.ContactData;
+
+public class ContactModificationTest extends TestBase{
+
+    @Test
+    public void testContactModification() {
+        app.getContactHelper().modificationSelectedContact();
+        app.getContactHelper().fillGroupForm(new ContactData("edit test1", "edit test2",
+                "edit test3", "edit test4", "edit test5", "edit test6", "edit test7", "edit test8"));
+        app.getContactHelper().submitContactModification();
+        app.getNavigationHelper().goToHomePage();
+    }
+}

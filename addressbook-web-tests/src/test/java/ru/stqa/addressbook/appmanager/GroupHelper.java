208764793afdd48103f4e567ab7b3319e9242984
Igor Golubenkov <i.golubenkov@mail.ru>
2016-09-25 19:40:19 +0300

    implemented inspection and enforcement preconditions

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/GroupHelper.java
@@ -35,4 +35,14 @@ public class GroupHelper extends HelperBase{
     public void submitGroupModification() {
         clickSearch(By.xpath("//input[@name='update']"));
     }
+    public void createGroup(GroupData group) {
+        initGroupCreation();
+        fillGroupForm(group);
+        submitGroupCreation();
+        returnToGroupPage();
+
+    }
+    public boolean isThereAGroup() {
+        return isElementPresent(By.xpath(".//*[@id='content']/form/span[1]/input"));
+    }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,17 @@
 package ru.stqa.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().goToGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+        }
+
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroup();
         app.getGroupHelper().returnToGroupPage();

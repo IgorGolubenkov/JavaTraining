48fb6ac88b5aa0086e77d2adf6bc2c5630cc95c1
Igor Golubenkov <i.golubenkov@mail.ru>
2016-09-25 09:36:10 +0300

    optimized page transitions

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/Navigationhelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/Navigationhelper.java
@@ -9,12 +9,20 @@ public class Navigationhelper extends HelperBase{
         super(wd);
     }
     public void goToGroupPage() {
+        if (isElementPresent(By.xpath("//h1[contains(.,'Groups')]")) &&
+                wd.findElement(By.xpath("//h1[contains(.,'Groups')]")).getText().equals("Group") &&
+                isElementPresent(By.xpath(".//*[@id='content']/form/input[1]"))) {
+            return;
+        }
         clickSearch(By.linkText("groups"));
     }
     public void goToAddandEditContactPage() {
         clickSearch(By.xpath("//a[contains(.,'add new')]"));
     }
     public void goToHomePage() {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
         clickSearch(By.xpath("//a[contains(.,'home')]"));
     }
 }

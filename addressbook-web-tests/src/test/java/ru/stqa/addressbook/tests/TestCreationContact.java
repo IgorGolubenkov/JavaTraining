38f03ff4e7feee954dc4214e0e4da8417f62e153
Igor Golubenkov <i.golubenkov@mail.ru>
2016-10-01 21:18:52 +0300

    fixed checking preconditions for creation: transition  to contact page after creation group

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestCreationContact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestCreationContact.java
@@ -13,7 +13,9 @@ public class TestCreationContact extends TestBase{
     public void testCreationContact() {
         app.getNavigationHelper().goToHomePage();
         if (! app.getContactHelper().thereIsAGroupForChoice()) {
+            app.getNavigationHelper().goToGroupPage();
             app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+            app.getNavigationHelper().goToHomePage();
         }
         List<ContactData> before = app.getContactHelper().getContactList();
         app.getNavigationHelper().goToAddandEditContactPage();

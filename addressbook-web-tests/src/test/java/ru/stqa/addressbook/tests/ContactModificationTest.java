a748b34ac8bb24c3bd8ebbc3961bd95512c4560c
Igor Golubenkov <i.golubenkov@mail.ru>
2016-10-01 22:30:05 +0300

    select modificatio contact index

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/ContactModificationTest.java
@@ -1,7 +1,10 @@
 package ru.stqa.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.addressbook.model.ContactData;
+
+import java.util.List;
 
 public class ContactModificationTest extends TestBase{
 
@@ -12,10 +15,13 @@ public class ContactModificationTest extends TestBase{
             app.getContactHelper().createContact(app, new ContactData("test1", "test2", "test3",
                     "test4", "test5", "test6", "test7", "test8", "test1"), true);
         }
-        app.getContactHelper().modificationSelectedContact();
+        List<ContactData> before = app.getContactHelper().getContactList();
+        app.getContactHelper().modificationSelectedContact(before.size() - 1);
         app.getContactHelper().fillContactForm(new ContactData("edit test1", "edit test2",
                 "edit test3", "edit test4", "edit test5", "edit test6", "edit test7", "edit test8", null), false);
         app.getContactHelper().submitContactModification();
         app.getNavigationHelper().goToHomePage();
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size());
     }
 }

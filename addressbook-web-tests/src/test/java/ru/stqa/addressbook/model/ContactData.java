bd2ee389e42261abf89efb88afbb4459a7314028
Igor Golubenkov <i.golubenkov@mail.ru>
2016-10-08 20:17:01 +0300

    add two constructor without, string representation, setter id, equels and hash for contact data

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/model/ContactData.java
@@ -1,6 +1,7 @@
 package ru.stqa.addressbook.model;
 
 public class ContactData {
+    private int id;
     private final String firstname;
     private final String middlename;
     private final String lastname;
@@ -13,6 +14,7 @@ public class ContactData {
 
     public ContactData(String firstname, String middlename, String lastname, String nickname, String title,
                        String company, String address, String homepage, String group) {
+        this.id = 0;
         this.firstname = firstname;
         this.middlename = middlename;
         this.lastname = lastname;
@@ -22,6 +24,23 @@ public class ContactData {
         this.address = address;
         this.homepage = homepage;
         this.group = group;
+    }
+
+    public ContactData(int id, String firstname, String middlename, String lastname, String nickname, String title,
+                       String company, String address, String homepage, String group) {
+        this.id = id;
+        this.firstname = firstname;
+        this.middlename = middlename;
+        this.lastname = lastname;
+        this.nickname = nickname;
+        this.title = title;
+        this.company = company;
+        this.address = address;
+        this.homepage = homepage;
+        this.group = group;
+    }
+    public int getId() {
+        return id;
     }
     public String getFirstname() {
         return firstname;
@@ -50,4 +69,43 @@ public class ContactData {
     public String getGroup() {
         return group;
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ContactData that = (ContactData) o;
+
+        if (id != that.id) return false;
+        if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+        if (lastname != null ? !lastname.equals(that.lastname) : that.lastname != null) return false;
+        return address != null ? address.equals(that.address) : that.address == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
+        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
 }
+

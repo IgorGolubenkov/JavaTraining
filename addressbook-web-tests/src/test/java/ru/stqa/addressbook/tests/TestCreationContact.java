16c4cc2e2aea8d0d2f4df291be9d99790d56d28e
Igor Golubenkov <i.golubenkov@mail.ru>
2016-10-08 20:18:34 +0300

    add checking compare sorted contact list

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestCreationContact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestCreationContact.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.addressbook.model.ContactData;
 import ru.stqa.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class TestCreationContact extends TestBase{
@@ -17,11 +18,22 @@ public class TestCreationContact extends TestBase{
         }
         List<ContactData> before = app.getContactHelper().getContactList();
         app.getNavigationHelper().goToAddandEditContactPage();
-        app.getContactHelper().fillContactForm(new ContactData("test1", "test2", "test3",
-                "test4", "test5", "test6", "test7", "test8", "test1"), true);
+        ContactData contact = new ContactData("test1", "test2", "test3",
+                "test4", "test5", "test6", "test7", "test8", "test254");
+        app.getContactHelper().fillContactForm(contact, true);
         app.getContactHelper().submitContactCreation();
         app.getNavigationHelper().goToHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+        contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+        before.add(contact);
+        Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
+
+
+
         }
 }

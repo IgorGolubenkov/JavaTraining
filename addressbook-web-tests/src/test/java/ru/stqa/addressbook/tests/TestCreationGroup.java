b54bb1e463d4e467101b67790a41dfa59b16f594
Golubenkov Igor <iogolubenkov@8bitov.com>
2016-10-17 09:41:19 +0300

    add provider test data, test build parametrize

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestCreationGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/TestCreationGroup.java
@@ -1,21 +1,34 @@
 package ru.stqa.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.addressbook.model.GroupData;
 import ru.stqa.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestCreationGroup extends TestBase {
 
-    @Test
-    public void testCreationGroup() throws InterruptedException {
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+        list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+        list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testCreationGroup(GroupData group) throws InterruptedException {
         //app.group().openNewTab();
         app.goTo().groupPage();
         Groups before = app.group().all();
         //int before = app.group().count();
-        GroupData group = new GroupData().withName("test254");
         app.group().create(group);
         //int after = app.group().count();
         assertThat(app.group().count(), equalTo(before.size() + 1));

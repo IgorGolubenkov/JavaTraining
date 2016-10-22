3cb92533b4e95a96a9ef6fb71480f559fbd7f6a6
Igor Golubenkov <i.golubenkov@mail.ru>
2016-10-22 13:39:57 +0300

    add test check address in contact form with a master page

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/ContactAddressTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/ContactAddressTests.java
@@ -1,6 +1,32 @@
 package ru.stqa.addressbook.tests;
 
-public class ContactAddressTests {
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.addressbook.model.ContactData;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ContactAddressTests extends TestBase{
+
+    @BeforeMethod
+    public void ensurePreconditions() throws InterruptedException {
+        app.goTo().goToHomePage();
+        if (! app.contact().isThereAContact()) {
+            app.contact().createContact(new ContactData().withAddress("   country Russia city Moscow, Lenina street apartment 1 apartment 1"), false);
+        }
+    }
+
+    @Test
+    public void testContactAddress() {
+        ContactData contact = app.contact().all().iterator().next();
+        ContactData contactInfoFromEditFrom = app.contact().infoFromEditFrom(contact);
+        assertThat(contact.getAddress(), equalTo(cleaned(contactInfoFromEditFrom.getAddress())));
+    }
+
+    public static String cleaned(String address) {
+        return address.replaceAll("^[\\s]*", "");
+    }
 
 }
 

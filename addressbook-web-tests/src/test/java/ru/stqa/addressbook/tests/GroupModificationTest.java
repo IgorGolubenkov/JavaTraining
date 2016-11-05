948ca6ba5427a7d866f1c4105fd71382ffead813
Igor Golubenkov <i.golubenkov@mail.ru>
2016-11-05 15:05:04 +0300

    edit group modify: parametrize and checked using get information from db

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/GroupModificationTest.java
@@ -1,14 +1,20 @@
 package ru.stqa.addressbook.tests;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
-import org.testng.Assert;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.addressbook.model.GroupData;
 import ru.stqa.addressbook.model.Groups;
 
-import java.util.Set;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -16,23 +22,55 @@ import static org.testng.Assert.assertEquals;
 
 public class GroupModificationTest extends TestBase{
 
-    @BeforeMethod
-    public void ensurePreconditions() {
-        app.goTo().groupPage();
-        if (app.group().all().size() == 0) {
-            app.group().create(new GroupData().withName("test1").withHeader("test2").withFooter("test3"));
+    @DataProvider
+    public Iterator<Object[]> modifyGroupsFromJSON() throws IOException {
+        try (BufferedReader readear = new BufferedReader(new FileReader
+                (new File("src/test/resources/TestData/groups/groups_modify.json")))) {
+            String json = "";
+            String line = readear.readLine();
+            while (line != null) {
+                json += line;
+                line = readear.readLine();
+            }
+            Gson gson = new Gson();
+            List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>() {
+            }.getType());
+            return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
         }
     }
 
-    @Test
-    public void testGroupModification() {
-        Groups before = app.group().all();
+    @BeforeMethod
+    public void ensurePreconditions() {
+        if (app.db().groups().size() == 0) {
+            app.goTo().groupPage();
+            app.group().create(new GroupData().withName("test1").withHeader("test2").withFooter("test3"));
+        }
+//        UI
+//        app.goTo().groupPage();
+//        if (app.group().all().size() == 0) {
+//            app.group().create(new GroupData().withName("test1").withHeader("test2").withFooter("test3"));
+//        }
+    }
+
+    @Test(dataProvider = "modifyGroupsFromJSON")
+    public void testGroupModification(GroupData group) {
+
+        Groups before = app.db().groups();
+
+//        Groups before = app.group().all();  // UI
+
         GroupData modifyGroup = before.iterator().next();
-        GroupData group = new GroupData().withName("edit test1").withHeader("edit test2")
-                .withFooter("edit test3").withId(modifyGroup.getId());
-        app.group().modify(group);
-        assertEquals(app.group().count(), before.size());
-        Groups after = app.group().all();
+        GroupData groupData = group.withId(modifyGroup.getId());
+
+        app.goTo().groupPage();
+        app.group().modify(groupData);
+
+        assertEquals(app.group().count(), before.size()); // UI hash
+
+        Groups after = app.db().groups();
+
+//        Groups after = app.group().all();   //UI
+
         assertThat(after, equalTo(before.withOut(modifyGroup).withAdded(group)));
     }
 
